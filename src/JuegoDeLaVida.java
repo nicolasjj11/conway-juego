bd418ec5e526d0bca6e584c2eb3e22d7db372801
nicolasjj11 <nicolasjaga700@gmail.com>
2024-10-05 08:00:50 -0500

    se optimizo el archivo juegoDeLavida

--- src/JuegoDeLaVida.java
+++ src/JuegoDeLaVida.java
@@ -3,55 +3,57 @@ import java.util.Scanner;
 
 public class JuegoDeLaVida {
     public static void main(String[] args) {
-        boolean[][] tablero = new boolean[5][5];
-        boolean[][] nuevoTablero = new boolean[5][5];
-        Random r = new Random();
         Scanner sc = new Scanner(System.in);
+        
+        // Solicitar tamaño del tablero
+        System.out.print("Ingrese el tamaño del tablero: ");
+        int n = sc.nextInt();
+        sc.nextLine();  // Limpiar el buffer
+
+        boolean[][] tablero = new boolean[n][n];
+        boolean[][] nuevoTablero = new boolean[n][n];
+        Random r = new Random();
 
         int gen = 0;
 
         // Inicializar tablero aleatoriamente
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 tablero[i][j] = r.nextBoolean();
             }
         }
-        
+
         while (true) {
             System.out.println("Generación " + gen);
             
             // Mostrar tablero
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     System.out.print(tablero[i][j] ? "■ " : "□ ");
                 }
                 System.out.println();
             }
 
             // Calcular siguiente generación
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     int vecinosVivos = 0;
                     for (int di = -1; di <= 1; di++) {
                         for (int dj = -1; dj <= 1; dj++) {
                             if (di == 0 && dj == 0) continue;
-                            int fi = (i + di + 5) % 5;
-                            int fj = (j + dj + 5) % 5;
+                            int fi = (i + di + n) % n;
+                            int fj = (j + dj + n) % n;
                             if (tablero[fi][fj]) vecinosVivos++;
                         }
                     }
-                    if (tablero[i][j]) {
-                        nuevoTablero[i][j] = vecinosVivos == 2 || vecinosVivos == 3;
-                    } else {
-                        nuevoTablero[i][j] = vecinosVivos == 3;
-                    }
+                    nuevoTablero[i][j] = (tablero[i][j] && (vecinosVivos == 2 || vecinosVivos == 3)) || (!tablero[i][j] && vecinosVivos == 3);
                 }
             }
 
-            // Actualizar tablero
-            for (int i = 0; i < 5; i++) {
-                System.arraycopy(nuevoTablero[i], 0, tablero[i], 0, 5);
-            }
+            // Intercambiar referencias de los tableros en lugar de copiar
+            boolean[][] temp = tablero;
+            tablero = nuevoTablero;
+            nuevoTablero = temp;
 
             gen++;
 
@@ -64,3 +66,4 @@ public class JuegoDeLaVida {
         sc.close();
     }
 }
+
